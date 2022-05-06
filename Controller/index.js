164bb6992e6fb80e74fde5ecfdb107ad3cc84b89
Hamza Ahmed Mughal <57133610+hamza19mughal99@users.noreply.github.com>
2022-05-07 01:27:25 +0500

    view api

--- Controller/index.js
+++ Controller/index.js
@@ -8,7 +8,15 @@ export const CreateUser = (req, res) => {
     res.status(201).send(adduser);
 }
 
-export const getAllUser = (req, res) => {
-    const userData = UserSchema.find();
-    res.status(200).json(userData)
+export const getAllUser = async (req, res) => {
+    const userData = await UserSchema.find();
+    res.status(200).send(userData)
+}
+
+export const getOneUser = async (req, res) => {
+    const {id} = req.params;
+
+    const getOneUser = await UserSchema.findById(id)
+
+    res.status(200).send(getOneUser)
 }
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import {CreateUser, getAllUser} from "../Controller/index.js";
+import {CreateUser, getAllUser, getOneUser} from "../Controller/index.js";
 
 
 router.post('/register', CreateUser);
-router.get('/all-users', getAllUser)
+router.get('/all-users', getAllUser);
+router.get('/all-users/:id', getOneUser)
 
 export default router;
